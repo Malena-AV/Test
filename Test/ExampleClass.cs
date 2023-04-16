e3aa53ec921d05284ac09108110e3c697aebf6e1
Malena-AV <m.alena07082003@gmail.com>
2023-04-16 18:17:34 +0300

    Добавьте файлы проекта.

---
+++ Test/ExampleClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class ExampleClass
+    {
+        public static int[,] Method(int[,] array, int value)
+        {
+            int[,] result = new int[array.GetLength(0), array.GetLength(1)];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    sum += array[i, j];
+                    if (sum == value)
+                    {
+                        result[i, j] = value;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ TestTests/ExampleClassTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Tests
+{
+    [TestClass()]
+    public class ExampleClassTests
+    {
+        [TestMethod()]
+        public void MethodTest()
+        {
+            int[,] array = new int[,]
+            {
+                { 1, 2 },
+                { 1, 2 }
+            };
+            int value = 3;
+
+            int[,] expected = new int[,]
+            {
+                { 0, 3 },
+                { 0, 3 }
+            };
+
+            int[,] actual = ExampleClass.Method(array, value);
+
+            for (int i = 0; i < actual.GetLength(0); i++)
+            {
+                for (int j = 0; j < actual.GetLength(1); j++)
+                {
+                    Assert.AreEqual(expected[i,j], actual[i,j]);
+                }
+
+            }
+        }
+    }
+}
